5d05a6b54b854486bbcd40fcedb334dd1d96ddd7
Justin M Wozniak <wozniak@mcs.anl.gov>
2013-10-02 14:00:38 -0500

    Proper generation ordering works

--- evs-sh-framework/evs.swift
+++ evs-sh-framework/evs.swift
@@ -2,6 +2,7 @@
 import files;
 import io;
 import string;
+import unix;
 
 app (file o) diffev_init()
 {
@@ -14,12 +15,16 @@ app (file o) discus_run(int members, int parameters,
   "./discus_run.sh" o members parameters cycle member;
 }
 
-app (void v) kuplot_run(file discus_output, int members, int parameters,
+app (file o) kuplot_run(file discus_output, int members, int parameters,
                         int cycle, int member)
 {
-  "./kuplot_run.sh" discus_output members parameters cycle member;
+  "./kuplot_run.sh" o discus_output members parameters cycle member;
 }
 
+app (file o) summarize(file kuplot_outputs[])
+{
+  "./kuplot_summarize.sh" o kuplot_outputs;
+}
 
 global const int CYCLES = 3;
 
@@ -35,18 +40,21 @@ main
   parameters = toint(p[1]);
   printf("members: %i parameters: %i", members, parameters);
 
-  void v[];
-  v[0] = make_void();
+  file v[];
+  v[0] = touch();
   
   for (int cycle = 0; cycle < CYCLES; cycle = cycle + 1)
   {
     wait (v[cycle])
     {
+      file kuplot_outputs[];
       foreach member in [0:members-1]
       {
         file discus_output = discus_run(members, parameters, cycle, member);
-        v[cycle+1] = kuplot_run(discus_output, members, parameters, cycle, member);
+        kuplot_outputs[member] =
+          kuplot_run(discus_output, members, parameters, cycle, member);
       }
+      v[cycle+1] = summarize(kuplot_outputs);
     }
   }
 }
