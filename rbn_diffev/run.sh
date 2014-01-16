35ba1ff0d9c84b6cbd0eab1e28d259ff51c98c1e
jmjwozniak <wozniak@mcs.anl.gov>
2014-01-16 17:32:24 -0600

    Set logging/debugging defaults; require cycle count on command line

--- rbn_diffev/run.sh
+++ rbn_diffev/run.sh
@@ -1,12 +1,23 @@
 #!/bin/bash -e
 
+if [[ ${#*} != 1 ]]
+then
+  echo "requires argument: cycles"
+  exit 1
+fi
+
+CYCLES=$1
+
 # Create directories required by the application:
 mkdir -p CALC DIFFEV FINAL
 
 stc -pu refine.swift
 export PYTHONPATH=${PYTHONPATH}:${PWD}
 
-export TURBINE_LOG=1 TURBINE_DEBUG=0
+# Logging/debugging off by default:
+export TURBINE_LOG=${TURBINE_LOG:-0}
+export TURBINE_DEBUG=${TURBINE_DEBUG:-0}
+export ADLB_DEBUG=${ADLB_DEBUG:-0}
 
 # Create an output file with an unused random number
 REFINE_OUT=""
@@ -16,5 +27,5 @@ do
   [[ ! -f ${REFINE_OUT} ]] && break
 done
 
-turbine -l -n 3 refine.tcl -cycles=3 |& tee ${REFINE_OUT}
-
+turbine -l -n 3 refine.tcl --cycles=${CYCLES} |& tee ${REFINE_OUT}
+echo "logged to: ${REFINE_OUT}"
