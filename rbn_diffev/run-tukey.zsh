f10d986359107b0c419f1dae3abf32ffbd88a8c8
Justin M Wozniak <wozniak@mcs.anl.gov>
2014-02-21 15:38:39 -0600

    Script to run DIFFEV on Tukey

---
+++ rbn_diffev/run-tukey.zsh
@@ -0,0 +1,56 @@
+#!/bin/zsh
+
+# Set cmake to use f2py -L Python/lib
+
+set -e
+
+DIFFUSE=${HOME}/proj/x86/diffuse/build
+
+if [[ ${#*} != 2 ]]
+then
+  echo "usage: run-tukey <procs> <cycles>"
+  exit 1
+fi
+
+PROCS=$1
+CYCLES=$2
+
+export QUEUE=${QUEUE:-default}
+export PPN=${PPN:-3}
+# Wall time in minutes
+WT=${WT:-60}
+
+# Logging/debugging off by default:
+export TURBINE_LOG=${TURBINE_LOG:-1}
+export TURBINE_DEBUG=${TURBINE_DEBUG:-0}
+export ADLB_DEBUG=${ADLB_DEBUG:-0}
+
+export PYTHONPATH=${PWD}
+export LD_LIBRARY_PATH=${PWD}:${HOME}/Public/sfw/x86/Python-2.7.6/lib:/gpfs/mira-fs0/software/x86_64/compilers/gcc/4.8.1/lib64
+# :${HOME}/.local/lib/python/
+export PATH=${HOME}/Public/sfw/x86/Python-2.7.6/bin:${PATH}
+
+set -u
+
+# Create directories required by the application:
+mkdir -p CALC DIFFEV FINAL
+
+cp -uv ${DIFFUSE}/**/*.so .
+
+echo "STC..."
+stc -pu refine.swift
+echo "done."
+
+# Must pick up user liblapack.so, libblas.so
+
+declare QUEUE
+
+export MODE=cluster
+export PROJECT=ExM
+export ADLB_PRINT_TIME=1
+
+turbine-cobalt-run.zsh -V -t 00:${WT}:00 -n ${PROCS} \
+  -i ${PWD}/turbine-init.sh \
+  -e PYTHONPATH=${PYTHONPATH} \
+  -e LD_LIBRARY_PATH=${LD_LIBRARY_PATH} \
+  refine.tcl --cycles=${CYCLES}
