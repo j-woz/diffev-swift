d06dcc27d70143db8612077b77de5e41899b918a
jmjwozniak <wozniak@mcs.anl.gov>
2014-03-11 09:58:43 -0500

    This script makes an AVI

---
+++ rbn_diffev/movie.avi.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+set -eu
+
+cd imgs
+
+print -l *.png > png.list
+
+mencoder mf://@png.list -mf w=800:h=600:fps=1:type=png \
+  -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -oac copy \
+  -o diffev.avi
